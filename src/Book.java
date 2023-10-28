a47a36277d667f62d859d20cee3dc880fb6264a6
User <drazinkin12@gmail.com>
2023-10-28 21:31:16 +0300

    2 commit

---
+++ src/Book.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+public class Book {
+    private final String bookName;
+    private final Author author;
+    private int publishYear;
+
+    public Book(String bookName, Author author, int publishYear) {
+        this.bookName = bookName;
+        this.author = author;
+        this.publishYear = publishYear;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(int publishYear) {
+        this.publishYear = publishYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(bookName, book.bookName) && Objects.equals(author, book.author);
+    }
+
+    public int hashCode() {
+        return Objects.hash(bookName, author);
+    }
+
+    public String toString() {
+        return "Book{" + "bookname='" + '\'' + ", author='" + author + '\'' + '}';
+    }
+}
